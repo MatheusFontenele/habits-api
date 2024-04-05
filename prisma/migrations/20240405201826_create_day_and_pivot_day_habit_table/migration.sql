043406f9587f68cf795dd8299809f67ca7d79034
MatheusFontenele <matheus.fontenele@icloud.com>
2024-04-05 17:35:43 -0300

    refactor: alter name of table day

--- prisma/migrations/20240405201826_create_day_and_pivot_day_habit_table/migration.sql
+++ prisma/migrations/20240405201826_create_day_and_pivot_day_habit_table/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "Day" (
+CREATE TABLE "days" (
     "id" TEXT NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Day_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "days_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -18,7 +18,7 @@ CREATE TABLE "day_habits" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Day_date_key" ON "Day"("date");
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "day_habits_habitId_dayId_key" ON "day_habits"("habitId", "dayId");
@@ -27,4 +27,4 @@ CREATE UNIQUE INDEX "day_habits_habitId_dayId_key" ON "day_habits"("habitId", "d
 ALTER TABLE "day_habits" ADD CONSTRAINT "day_habits_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "habits"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "day_habits" ADD CONSTRAINT "day_habits_dayId_fkey" FOREIGN KEY ("dayId") REFERENCES "Day"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "day_habits" ADD CONSTRAINT "day_habits_dayId_fkey" FOREIGN KEY ("dayId") REFERENCES "days"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
