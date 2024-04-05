a7b95bf0140ba6eab1831f95feb64c809efa002a
MatheusFontenele <matheus.fontenele@icloud.com>
2024-04-05 17:06:13 -0300

    chore: add fastify cors

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import cors from "@fastify/cors";
 import { fastifyJwt } from "@fastify/jwt";
 import fastify from "fastify";
 import { env } from "./config/env";
@@ -8,5 +9,6 @@ export const app = fastify();
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET as string, 
 })
+app.register(cors);
 app.register(user, { prefix: "/api/v1/user" });
 app.register(auth, { prefix: "/api/v1/auth" });
